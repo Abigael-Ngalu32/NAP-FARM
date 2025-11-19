18951339d6642ed09a47f3cff89757685888934b
Abigael Ngalu <abigaelmusebe@gmail.com>
2025-11-19 13:05:20 +0000

    update changes

--- nap-farm/src/pages/Home.jsx
+++ nap-farm/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import BreedGrid from "../components/BreedGrid";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <BreedGrid />
+      <Footer />
     </>
   );
 }
