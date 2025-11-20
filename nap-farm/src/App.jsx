6a5aba34604b7c87766ed3d89070a44111fa757b
Abigael Ngalu <abigaelmusebe@gmail.com>
2025-11-20 09:09:03 +0000

    update changes

--- nap-farm/src/App.jsx
+++ nap-farm/src/App.jsx
@@ -1,9 +1,11 @@
 export default function App() {
   return (
     <div className="text-3xl text-center mt-20 text-green-600">
-      If you see this, App is working!
+      Hello from Nap Farm 🐔
     </div>
   );
 }
+
+
 
 
